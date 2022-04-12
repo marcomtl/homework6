4931f5f9d635237add17cb8a615c4d565c6f3267
marcomtl <97364697+marcomtl@users.noreply.github.com>
2022-04-12 16:13:08 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,5 +1,6 @@
 var video = document.querySelector("#player1");
-
+video.autoplay = false;
+video.loop = false;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
@@ -7,24 +8,27 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	video.play()
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
-	video.pause()
+	video.pause();
 });
 
 
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow down video");
 	video.playbackRate *= 0.95;
+	console.log(video.playbackRate)
 });
 
 
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed up video");
 	video.playbackRate /= 0.95;
+	console.log(video.playbackRate)
 });
 
 
@@ -34,22 +38,35 @@ document.querySelector("#skip").addEventListener("click", function() {
 	if (video.currentTime >= video.duration){
 		video.currentTime = 0;
 	}
+	console.log(video.currentTime)
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
 	console.log("mute video");
-	video.muted = true
+	if (video.muted === false) {
+		video.muted = true;
+ }
+ else {
+	video.muted = false};
 });
+
+
 
 document.querySelector("#slider").addEventListener("click", function() {
 	console.log("change volume with slider");
+	video.volume = this.value /100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+	console.log(video.volume);
+	// change volume; changing text to match
 });
 
 document.querySelector("#vintage").addEventListener("click", function() {
+	video.
 	console.log("Use oldSchool class");
 });
 
-document.querySelector("#original").addEventListener("click", function() {
+document.querySelector("#orig").addEventListener("click", function() {
+	// video.
 	console.log("revert to original");
 });
 
